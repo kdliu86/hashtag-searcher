cc83fb3f2daac5f6feee49f82ed84c02194e69af
kdliu86 <ken.d.liu@gmail.com>
2014-05-21 23:16:29 -0400

    Update hashtag.py

--- hashtag.py
+++ hashtag.py
@@ -1,4 +1,5 @@
 from twython import Twython
+from pprint import pprint
 
 APP_KEY = ""
 APP_SECRET = ""
@@ -9,14 +10,16 @@ def hashtag():
     twitter = Twython(APP_KEY, APP_SECRET,
                         OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     # search_term = raw_input("What hashtag would you like to search for? ")
-    search_term = ""
+    search_term = "#thatswhatshesaid"
     d = twitter.search(q=search_term, count=100)
     counter = 0
 
     for tweet in d['statuses']:
         counter += 1
-        print tweet['text']
-
-    print 'There are at least %i tweets for hashtag %s %s' % (counter, search_term, tweettext)
+        try:
+            pprint(str(tweet['text']))
+        except:
+            pprint(tweet['text'])
+    print 'There are at least %i tweets for hashtag %s' % (counter, search_term)
 
 hashtag()
